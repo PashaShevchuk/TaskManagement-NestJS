7f5d2edd3758055375740870bba84051d495017b
pavlo-shevchuk <pavlo.shevchuk@sombrainc.com>
2021-05-18 20:45:46 +0300

    feat: create functionality for authentication, set up JWT/Passport.js

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,16 +1,36 @@
-import { Injectable } from '@nestjs/common';
-import { UserRepository } from './user.repository';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
 import { InjectRepository } from '@nestjs/typeorm';
+import { UserRepository } from './user.repository';
 import { AuthCredentialsDto } from './dto/auth-credentials.dto';
+import { JwtPayload } from './jwt-payload.interface';
 
 @Injectable()
 export class AuthService {
   constructor(
     @InjectRepository(UserRepository)
     private userRepository: UserRepository,
+    private jtwService: JwtService,
   ) {}
 
   async signUp(authCredentialsDto: AuthCredentialsDto): Promise<void> {
     return this.userRepository.signUp(authCredentialsDto);
+  }
+
+  async signIn(
+    authCredentialsDto: AuthCredentialsDto,
+  ): Promise<{ accessToken: string }> {
+    const username = await this.userRepository.validateUserPassword(
+      authCredentialsDto,
+    );
+
+    if (!username) {
+      throw new UnauthorizedException('Invalid credentials');
+    }
+
+    const payload: JwtPayload = { username };
+    const accessToken = await this.jtwService.sign(payload);
+
+    return { accessToken };
   }
 }
