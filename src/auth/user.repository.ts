2e8e795a86428118d121ba9a9f120f3da662609d
pavlo-shevchuk <pavlo.shevchuk@sombrainc.com>
2021-05-18 19:37:16 +0300

    feat: create functionality for hash password

--- src/auth/user.repository.ts
+++ src/auth/user.repository.ts
@@ -1,4 +1,9 @@
 import { EntityRepository, Repository } from 'typeorm';
+import {
+  ConflictException,
+  InternalServerErrorException,
+} from '@nestjs/common';
+import * as bcrypt from 'bcrypt';
 import { User } from './user.entity';
 import { AuthCredentialsDto } from './dto/auth-credentials.dto';
 
@@ -9,8 +14,22 @@ export class UserRepository extends Repository<User> {
 
     const user = new User();
     user.username = username;
-    user.password = password;
+    user.salt = await bcrypt.genSalt();
+    user.password = await this.hashPassword(password, user.salt);
 
-    await user.save();
+    try {
+      await user.save();
+    } catch (error) {
+      // 23505 - duplicate username
+      if (error.code === '23505') {
+        throw new ConflictException('Username already exist');
+      } else {
+        throw new InternalServerErrorException();
+      }
+    }
+  }
+
+  private async hashPassword(password: string, salt: string): Promise<string> {
+    return bcrypt.hash(password, salt);
   }
 }
